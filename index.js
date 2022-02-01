612f02455cc3e4bad6ffe5d46ad1bdd536e4c5ae
Anonymous <ParsWebDesigners@gmail.com>
2022-02-01 08:28:31 +0330

    make responsibility

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const $=el=>{return document.querySelector(el);}
 
 var value=0;
-setInterval(x,2000)
-function x(){
+setInterval(temp,2000)
+function temp(){
     $('.speedmeter__hand').style=`transform: translate(-50%,-10px) rotateZ(${value +270}deg);`;
     value=(value+18>180)? 0 : (value+18);
 }
 
-
-setTimeout(y,5000);
-function y(){
+setTimeout(Sun,5000);
+function Sun(){
     $('#dark').style="display:block;";
     $('#dark-pluk').style="display:block;";
     $('.bi-moon-stars-fill').style="display:block;";
