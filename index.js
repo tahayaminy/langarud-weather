2676d63e065b092ebe6c62795bddf46b8169bea4
Anonymous <ParsWebDesigners@gmail.com>
2022-02-03 23:27:06 +0330

    DAY BREAK its ok!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const $=el=>{return document.querySelector(el);}
 const mock={"upTemp":20,"downTemp":0,"currentTemp":11,"sunset":['19','41'],"sunrise":['07','41'],"sunriseT":['6','35'],"sunsetT":['19','40']};
-const current=[23,59];
+const current=[6,35];
 const MIN=time=>{return (time[0]*60)+Number(time[1])};
 var daybreak=[24,0]
 const DayBreak= current=>{daybreak[0]+=current[0];daybreak[1]=current[1];}
@@ -21,24 +21,11 @@ $('#sunset span:last-child').innerText=mock.sunset[1];
 var gold;
 var sun;
 var dark;
-var distance;
+var distance=(MIN([23,59]) - MIN(mock.sunset)) + MIN(mock.sunriseT);
 
 if(MIN(current)>=0 && MIN(current)<=MIN(mock.sunriseT)){
     DayBreakFlag=true;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if(MIN(current)>MIN(mock.sunset)){
     //OK
@@ -47,8 +34,7 @@ if(MIN(current)>MIN(mock.sunset)){
     $('#gold').style.strokeDashoffset='-383';
     $('.cont-sun--moon').style.transform='rotate(450deg)'; 
 
-              
-    distance=(MIN([23,59]) - MIN(mock.sunset)) + MIN(mock.sunriseT);
+    
     sunMoon=((MIN(current)-MIN(mock.sunset))*180)/(distance+1);
     setTimeout(Night,2000);
 
@@ -69,25 +55,17 @@ if(MIN(current)>MIN(mock.sunset)){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 else if(DayBreakFlag){
+    //OK
     console.log('Day Break!');
     DayBreak(current);
-    $('#gold').style.strokeDashoffset='-383';
 
-    sun=((MIN(daybreak)-MIN(mock.sunset))*360)/(24*60);
+    //COMPELETE DAY
+    $('#gold').style.strokeDashoffset='-383';
     $('.cont-sun--moon').style.transform='rotate(450deg)'; 
 
+    
+    sunMoon=((MIN(daybreak)-MIN(mock.sunset))*180)/(distance+1);
     setTimeout(Sun,2000);
     function Sun(){
         $('#dark').style="opacity:1;";
@@ -96,13 +74,25 @@ else if(DayBreakFlag){
         $('.bi-sun-fill').style="display:none;";
           
 
-        dark=((MIN(daybreak)-MIN(mock.sunset))*383)/(12*60);
-        $('.cont-sun--moon').style.transform=`rotate(${sun+450}deg)`; 
+        dark=((MIN(daybreak)-MIN(mock.sunset))*383)/(distance+1);
+        $('.cont-sun--moon').style.transform=`rotate(${sunMoon+450}deg)`; 
         $('#dark').style.strokeDashoffset=`${383-dark}`;
     }
     $('#sunrise span:first-child').innerText=mock.sunriseT[0];
     $('#sunrise span:last-child').innerText=mock.sunriseT[1];
-}else{
+}
+
+
+
+
+
+
+
+
+
+
+
+else{
     //OK
     console.log('Is Day!'); 
     gold=((MIN(current)-MIN(mock.sunrise))*383)/(MIN(mock.sunset)-MIN(mock.sunrise));
